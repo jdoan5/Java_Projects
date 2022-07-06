54a71927cad59c9649a9e6b14b1421aeb22ff357
John Doan <30330279+jdoan5@users.noreply.github.com>
2022-07-06 15:34:11 -0400

    Password_Validation.java

--- src/Password_Validation.java
+++ src/Password_Validation.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+import java.util.regex.*;
 public class Password_Validation {
     public static void main(String[] args) {
         //Enter password
+        Scanner input = new Scanner(System.in);
         System.out.println("Please enter the password: ");
+
+        //Read user input
+        String userInput = input.nextLine();
 
     }
 
