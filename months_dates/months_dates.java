61968e27d04726678ee084d7353168cf70c6ac9f
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-02-16 12:28:18 -0500

    Update months_dates.java

--- months_dates/months_dates.java
+++ months_dates/months_dates.java
@@ -1,12 +1,24 @@
-package BMI_Calculator;
+// Display list of the month from Jan. to Dec.
 
-public class BMI_calculator {
+package months_dates;
+
+public class months_dates {
     public static void main(String args[]) {
-        int[] daysInMonth = {31,2,3,4,5,6,7,8,9,10,11,12};
+        int[] daysInMonth = {31,28,31,30,31,30,31,30,31,30,31,30};
         String[] monthsName = {"January","February","March","April","May","June","July","August","September","October","November","December"};
 
-        System.out.println(monthsName[0] + " has "+ daysInMonth[0] + " days.");
-        System.out.println(monthsName[1] + " has "+ daysInMonth[1] + " days.");
-        System.out.println(monthsName[2] +  "has "+ daysInMonth[2] + " day.");
+        System.out.println(monthsName[0] + " has " + daysInMonth[0] + " days.");
+        System.out.println(monthsName[1] + " has " + daysInMonth[1] + " days.");
+        System.out.println(monthsName[2] +  " has " + daysInMonth[2] + " day.");
+        System.out.println(monthsName[3] + " has " + daysInMonth[3] + " days.");
+        System.out.println(monthsName[4] + " has " + daysInMonth[5] + " days. ");
+        System.out.println(monthsName[5] + " has " + daysInMonth[5] + " days.");
+        System.out.println(monthsName[6] + " has " + daysInMonth[6] + " days. ");
+        System.out.println(monthsName[7] + " has " + daysInMonth[7] + " days.");
+        System.out.println(monthsName[8] + " has " + daysInMonth[8] + " days. ");
+        System.out.println(monthsName[9] + " has " + daysInMonth[9] + " days. ");
+        System.out.println(monthsName[10] + " has " + daysInMonth[10] + " days.");
+        System.out.println(monthsName[11] + " has " + daysInMonth[11] + " days.");
+
     }
 }
