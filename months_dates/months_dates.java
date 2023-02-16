caf205f462e9dcd403d5b7dcec97ff36c83f00b3
John Doan <30330279+jdoan5@users.noreply.github.com>
2023-02-16 09:50:59 -0500

    Display month in array

---
+++ months_dates/months_dates.java
@@ -0,0 +1,12 @@
+package BMI_Calculator;
+
+public class BMI_calculator {
+    public static void main(String args[]) {
+        int[] daysInMonth = {31,2,3,4,5,6,7,8,9,10,11,12};
+        String[] monthsName = {"January","February","March","April","May","June","July","August","September","October","November","December"};
+
+        System.out.println(monthsName[0] + " has "+ daysInMonth[0] + " days.");
+        System.out.println(monthsName[1] + " has "+ daysInMonth[1] + " days.");
+        System.out.println(monthsName[2] +  "has "+ daysInMonth[2] + " day.");
+    }
+}
